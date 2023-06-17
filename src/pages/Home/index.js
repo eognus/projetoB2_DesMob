35b0598326d4d1bd8df6b014bef5f030f840d311
eognus <103257622+eognus@users.noreply.github.com>
2023-06-17 09:48:59 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,17 @@
 import { Link } from "@react-navigation/native";
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import styles from "./style";
 
 
 export default function HomePage(){
 
    return(
-    <View>
-        <Link to={{screen: 'Cadastro'}}>Cadastrar</Link>
-        <Link to={{screen: 'Login'}}>Login</Link>
+    <View style={styles.container}>
+        <Text style={styles.titulo}> Escolha uma opção para logar na página </Text>
+        <View style={styles.opcoes}>
+            <Link style={styles.btnopcao} to={{screen: 'Cadastro'}}>Cadastro</Link>
+            <Link style={styles.btnopcao} to={{screen: 'Login'}}>Login</Link>
+        </View>
     </View>
    ) 
 }
